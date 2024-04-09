5f1448fb51432d024aef5fcc70a0a76756124695
Danilo Stojanovic <danilo@Danilos-MacBook-Pro.local>
2024-04-09 22:11:29 +0200

    add: add workbox mode

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,9 @@ const withPWA = withPWAInit({
   disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
   register: true, // Register the PWA service worker
   skipWaiting: true, // Skip waiting for service worker activation
+  workboxOptions: {
+    mode: "production",
+  },
 });
 
 const nextConfig = {
