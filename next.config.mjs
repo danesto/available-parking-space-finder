dc543c836260954d2579b2f0000d58d5db758efe
Danilo Stojanovic <danilo@Danilos-MacBook-Pro.local>
2024-04-09 21:41:19 +0200

    add: pwa config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
+import pwa from "next-pwa";
+
+const withPWA = pwa({
+  dest: "public", // Destination directory for the PWA files
+  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+  register: true, // Register the PWA service worker
+  skipWaiting: true, // Skip waiting for service worker activation
+});
+
 const nextConfig = {
   logging: {
     fetches: true,
   },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
+import type { Viewport } from "next";
 import "./globals.css";
 
 const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
@@ -13,6 +14,23 @@ export const metadata: Metadata = {
     description:
       "Слободна места на јавним паркиралиштима Београда | Available places in public parking lots in Belgrade",
   },
+  manifest: "/manifest.json",
+  authors: [
+    { name: "Danilo Stojanovic" },
+    {
+      name: "Danilo Stojanovic",
+      url: "https://github.com/danesto/available-parking-space-finder",
+    },
+  ],
+  icons: [
+    { rel: "apple-touch-icon", url: "icons/apple-touch-icon.png" },
+    { rel: "icon", url: "icons/favicon-192x192.png" },
+  ],
+};
+
+export const viewport: Viewport = {
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
+  viewportFit: "cover",
 };
 
 export default function RootLayout({
